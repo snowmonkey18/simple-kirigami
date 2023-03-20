d978b124679ef7598700e87dc4743a8355fe0c92
snowmonkey18 <snowmonkey.gt@gmail.com>
2023-03-20 13:51:10 -0400

    make function get_squares and squares_to_data which don't account for overlapping vertices

--- main.cpp
+++ main.cpp
@@ -1,5 +1,66 @@
 #include <igl/opengl/glfw/Viewer.h>
 
+void get_squares(int n, std::vector<std::vector<float>> *squares) {
+    /*
+    * n [>= 1]: size of grid (for nxn grid of 1x1 squares)
+    * return: vector of coordinates of locations of squares (by bottom - left vertex)
+    */
+    std::vector<std::vector<float>> squares(1, { 0.f, 0.f, 0.f }); // first square at origin
+    for (int i = 1; i < n; i++) {
+        std::vector<float> prev = (*squares)[squares->size() - 1];
+        // for (i=1) and subsequent odd iterations: go "out right" -> up -> left
+        if (i % 2 == 1) {
+            squares->push_back({ prev[0] + 1, prev[1], prev[2] }); // go "out right"
+            for (int j = 1; j <= i; j++) {
+                squares->push_back({ prev[0] + 1, prev[1] + j, prev[2] }); // go up
+            }
+            for (int j = 0; j < i; j++) {
+                squares->push_back({ prev[0] - j, prev[1] + i, prev[2] }); // go left
+            }
+        }
+        // on (i=2) and subsequent even iterations: go "out up" -> right -> down
+        else {
+            squares->push_back({ prev[0], prev[1] + 1, prev[2] }); // go "out up"
+            for (int j = 1; j <= i; j++) {
+                squares->push_back({ prev[0] + j, prev[1] + 1, prev[2] }); // go right
+            }
+            for (int j = 0; j < i; j++) {
+                squares->push_back({ prev[0] + i, prev[1] - j, prev[2] }); // go down
+            }
+        }
+    }
+}
+
+void squares_to_data(std::vector<std::vector<float>> *squares, std::vector<std::vector<float>> *vertices, std::vector<std::vector<int>> *faces) {
+    std::vector<std::vector<float>> vertices;
+    for (int i = 0; i < squares->size(); i++) {
+        float x = (*squares)[i][0];
+        float y = (*squares)[i][1];
+        float z = (*squares)[i][2];
+        std::vector<float> v0;
+        std::vector<float> v1;
+        std::vector<float> v2;
+        std::vector<float> v3;
+
+        // origin square has all new vertices
+        // 
+        v0 = { x, y, z };
+        v1 = { x + 1, y, z };
+        v2 = { x + 1, y + 1, z };
+        v3 = { x, y + 1, z };
+
+
+        // 1st triangle
+        vertices->push_back(v0);
+        vertices->push_back(v1);
+        vertices->push_back(v3);
+        // 2nd triangle
+        vertices->push_back(v1);
+        vertices->push_back(v2);
+        vertices->push_back(v3);
+    }
+}
+
 int main(int argc, char* argv[])
 {
     //// mesh [old]
@@ -29,38 +90,12 @@ int main(int argc, char* argv[])
     //    7, 9, 10).finished();
 
     // mesh [new]: nxn grid of 1x1 squares
-    const int n = 1;
-    // squares: vector of coordinates of locations of squares (by bottom-left vertex)
-    std::vector<std::vector<float>> squares(1, { 0.f, 0.f, 0.f}); // first square at origin
-    for (int i = 1; i < n; i++) {
-        std::vector<float> prev = squares[squares.size() - 1];
-        // for (i=1) and subsequent odd iterations: go "out right" -> up -> left
-        if (i % 2 == 1) {
-            squares.push_back({ prev[0] + 1, prev[1], prev[2]}); // go "out right"
-            for (int j = 1; j <= i; j++) {
-                squares.push_back({ prev[0] + 1, prev[1] + j, prev[2] }); // go up
-            }
-            for (int j = 0; j < i; j++) {
-                squares.push_back({ prev[0] - j, prev[1] + i, prev[2] }); // go left
-            }
-        } 
-        // on (i=2) and subsequent even iterations: go "out up" -> right -> down
-        else {
-            squares.push_back({ prev[0], prev[1] + 1, prev[2] }); // go "out up"
-            for (int j = 1; j <= i; j++) {
-                squares.push_back({ prev[0] + j, prev[1] + 1, prev[2] }); // go right
-            }
-            for (int j = 0; j < i; j++) {
-                squares.push_back({ prev[0] + i, prev[1] - j, prev[2] }); // go down
-            }
-        }
-    }
-
-    /*for (int i = 0; i < squares.size(); i++) {
-        for (int j = 0; j < squares[0].size(); j++)
-            std::cout << squares[i][j] << std::endl;
-    }
-    std::cout << "size" << squares.size() << std::endl;*/
+    int n = 1;
+    std::vector<std::vector<float>> squares;
+    std::vector<std::vector<float>> vertices;
+    std::vector<std::vector<int>> faces;
+    get_squares(n, &squares);
+    squares_to_data(&squares, &vertices, &faces);
 
 //    std::vector<std::vector<float>> pre_V;
 //    std::vector<std::vector<int>> pre_F;
