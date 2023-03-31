b64dea7da9e0de68cfd29f4dc82597074f23461f
snowmonkey18 <snowmonkey.gt@gmail.com>
2023-03-30 23:24:43 -0400

    current_mouse_x/y print sensible numbers real time, but not in desired range, and update not working

--- main.cpp
+++ main.cpp
@@ -3,72 +3,42 @@
 
 int main(int argc, char* argv[])
 {
-    float theta = .5;
-    float s = sin(theta);
-    float c = cos(theta);
-    float z = 0.f;
+    Eigen::MatrixXd V = (Eigen::MatrixXd(3, 3) <<
+        0.f, 0.f, 0.f,
+        1.f, 0.f, 0.f,
+        0.f, 1.f, 0.f
+        ).finished();
+    Eigen::MatrixXi F = (Eigen::MatrixXi(1, 3) <<
+        0, 1, 2
+        ).finished();
+    igl::opengl::glfw::Viewer viewer;
 
-    float x00 = 0.f;
-    float y00 = 0.f;
-    float x01 = x00 + 1.f;
-    float y01 = y00;
-    float x02 = x01;
-    float y02 = y01 + 1.f;
-    float x03 = x02 - 1.f;
-    float y03 = y02;
+    viewer.callback_mouse_down =
+        [&V, &F](igl::opengl::glfw::Viewer& viewer, int, int)->bool
+    {
+        int fid;
+        Eigen::Vector3f bc;
+        // Cast a ray in the view direction starting from the mouse position
+        double x = viewer.current_mouse_x;
+        double y = viewer.core().viewport(3) - viewer.current_mouse_y;
+        double z = 0.f;
 
-    float x10 = x02 + s;
-    float y10 = y02 - c;
-    float x11 = x10 + c;
-    float y11 = y10 + s;
-    float x12 = x11 - s;
-    float y12 = y11 + c;
+        std::cout << "hello: " << x << ' ' << y << ' ' << z << std::endl;
 
-    float x20 = x12 - 1.f;
-    float y20 = y12 + 0.f;
-    float x22 = x12 + 0.f;
-    float y22 = y12 + 1.f;
-    float x23 = x22 - 1.f;
-    float y23 = y22 + 0.f;
+        V = (Eigen::MatrixXd(3, 3) <<
+            0.f, 0.f, z,
+            1.f, 0.f, z,
+            x, y, z
+            ).finished();
 
-    float x32 = x20 - s;
-    float y32 = y20 + c;
-    float x33 = x32 - c;
-    float y33 = y32 - s;
-    
-    const Eigen::MatrixXd V = (Eigen::MatrixXd(12, 3) << 
-        x00, y00, z,
-        x01, y01, z,
-        x02, y02, z,
-        x03, y03, z,
+        F = (Eigen::MatrixXi(1, 3) <<
+            0, 1, 2
+            ).finished();
 
-        x10, y10, z,
-        x11, y11, z,
-        x12, y12, z,
-        //xy13 = xy02
-
-        x20, y20, z,
-        //xy21 = xy12
-        x22, y22, z,
-        x23, y23, z,
-
-        //xy30 = xy13
-        //xy31 = xy20
-        x32, y32, z,
-        x33, y33, z).finished();
-
-    const Eigen::MatrixXi F = (Eigen::MatrixXi(8, 3) <<
-        0, 1, 3,
-        1, 2, 3,
-        4, 5, 2,
-        5, 6, 2,
-        7, 6, 9,
-        6, 8, 9,
-        3, 7, 11,
-        7, 10, 11).finished();
+        return false;
+    };
 
     // show
-    igl::opengl::glfw::Viewer viewer;
     viewer.data().set_mesh(V, F);
     viewer.data().set_face_based(true);
     viewer.launch();
